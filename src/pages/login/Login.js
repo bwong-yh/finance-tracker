9f615084ae63b1fb5597f4637304e992d7d8c92f
Billy Wong <bwong.yh@protonmail.com>
2022-11-14 21:04:10 +0900

    create login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,20 @@
 import './Login.css';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <form className='login-form'>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input type='email' name='' id='' />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input type='password' name='' id='' />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
+  );
 };
 
 export default Login;
