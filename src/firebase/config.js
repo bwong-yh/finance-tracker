5d71c7df4d3c085238c001e0be4683d20b039768
Billy Wong <bwong.yh@protonmail.com>
2022-11-14 21:33:04 +0900

    set up firebase with node

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsOH5C5vGoDbhYLyVnD9mCLtqb1vKcXGI',
+  authDomain: 'moneypath-f93f5.firebaseapp.com',
+  projectId: 'moneypath-f93f5',
+  storageBucket: 'moneypath-f93f5.appspot.com',
+  messagingSenderId: '572296809277',
+  appId: '1:572296809277:web:a599bafa7d1b10eff01354',
+};
+
+// init firebase & firestore
+initializeApp(firebaseConfig);
+const db = getFirestore();
+
+export { db };
