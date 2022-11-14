a73198a893abe3641f4b54cd715718403bdf2bca
Billy Wong <bwong.yh@protonmail.com>
2022-11-14 20:51:17 +0900

    create routes to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// pages
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className='App'>
-      <h1>React</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='*' element={<Navigate to='/' />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
